0f56b4087d4c2319f9c5856bde512c1e0b200ab2
Tomo Ferega <tomo@element.hr>
2014-06-19 10:20:40 +0200

    SAPI: Added getMap and select.

--- code/ScalaApi/src/main/scala/com/ferega/props/sapi/PropsLoader.scala
+++ code/ScalaApi/src/main/scala/com/ferega/props/sapi/PropsLoader.scala
@@ -3,6 +3,8 @@ package sapi
 
 import java.util.Optional
 
+import scala.collection.JavaConversions._
+
 object PropsLoader {
   private def optionalToOption[T](jopt: Optional[T]): Option[T] =
     if (jopt.isPresent) {
@@ -31,4 +33,10 @@ class PropsLoader(val useSystemProps: Boolean, val resolvablePathList: japi.Prop
 
     def get[T](key: String)(implicit ev: ValueConverter[T]): T =
       opt[T](key)(ev).getOrElse(throw new IllegalArgumentException(s"""Key "$key" not found in any of defined properties"""))
+
+    lazy val map: Map[String, String] =
+      baseLoader.getMap.toMap;
+
+    def select(prefix: String): Map[String, String] =
+      baseLoader.select(prefix).toMap
 }
